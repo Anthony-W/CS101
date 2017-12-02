baacca54b59638b45d1680060b1853dfe3dc6438
Unknown <thebobcrafterguy@gmail.com>
2017-12-02 17:47:30 -0600

    Commented Savings and slight adjustments to Account

--- Project 6/src/Account.java
+++ Project 6/src/Account.java
@@ -6,154 +6,159 @@
 */
 public class Account implements Filterable
 {
-	//current balance of this account
-    protected double _balance;
-    public double getBalance() { return _balance; }
-    
-    //name of this account
-    protected String _name;
-    public String getName() { return _name; }
-    
-    //account linked to this one
-    protected Account _link;
-    public Account getLink() { return _link; }
+	// current balance of this account
+	protected double _balance;
+	public double getBalance() { return _balance; }
 
+	// name of this account
+	protected String _name;
+	public String getName() { return _name; }
 
-    /*
-     * default constructor
-     */
-    public Account() {
-    	this("");
-    }
-    
-    
-    /*
-     * overloaded constructor
-     * takes the name of the account as a parameter
-     */
-    public Account(String name) {
-    	_name = name;
-    	_balance = 0;
-    	_link = null;
-    }
-    
-    
-    
-    
-    /*
-     * links this  account to another account, and vice versa
-     * 
-     * @param linkAcct		account to link to
-     * @return whether or not the new link was established
-     */
-    public boolean link(Account linkAcct)
-    {
-    	if (linkAcct == null) return false;
-    	if (this == linkAcct) return false;
-    	
-    	unlink();
+	// account linked to this one
+	protected Account _link;
+	public Account getLink() { return _link; }
+
+	/*
+	 * default constructor
+	 */
+	public Account()
+	{
+		this("");
+	}
+
+	/*
+	 * overloaded constructor
+	 * 
+	 * @param name		name of account
+	 */
+	public Account(String name)
+	{
+		_name = name;
+		_balance = 0;
+		_link = null;
+	}
+
+	/*
+	 * links this account to another account, and vice versa
+	 * 
+	 * @param linkAcct account to link to
+	 * 
+	 * @return whether or not the new link was established
+	 */
+	public boolean link(Account linkAcct)
+	{
+		if (linkAcct == null)
+			return false;
+		if (this == linkAcct)
+			return false;
+
+		unlink();
 		_link = linkAcct;
 		linkAcct._link = this;
 		return true;
-    }
-    
-    
-    /*
-     * adds money to this account
-     * 
-     * @param amt 	amount to be deposited
-     */
-    public void deposit(double amt) throws LinkAccountException
-    {
-    	_balance += amt;
-    }
-    
+	}
 
-    /*
-     * removes money from this account
-     * 
-     * @param amt	amount to be withdrawn
-     */
-    public void withdraw(double amt) throws InsufficientFundsException
-    {
-    	if (_balance >= amt)
-    	{
-    		_balance -= amt;
-    	}
-    	else
-    	{
-    		if (_link == null) throw new InsufficientFundsException("Overdraft with no linked account");
-    		if (_balance + _link._balance < amt) throw new InsufficientFundsException("Not enough funds in accounts");
-	    	
-    		amt -= _balance;
-    		_balance = 0;
-    		_link.withdraw(amt);
-    	}
-    }
-    
-    
-    /*
-     * removes any link from this account
-     */
-    public void unlink()
-    {
-    	if (_link == null) return;
-    	
-    	_link._link = null;
+	/*
+	 * adds money to this account
+	 * 
+	 * @param amt amount to be deposited
+	 */
+	public void deposit(double amt) throws LinkAccountException
+	{
+		_balance += amt;
+	}
+
+	/*
+	 * removes money from this account
+	 * 
+	 * @param amt amount to be withdrawn
+	 */
+	public void withdraw(double amt) throws InsufficientFundsException
+	{
+		if (_balance >= amt)
+		{
+			_balance -= amt;
+		} else
+		{
+			if (_link == null)
+				throw new InsufficientFundsException("Overdraft with no linked account");
+			if (_balance + _link._balance < amt)
+				throw new InsufficientFundsException("Not enough funds in accounts");
+
+			amt -= _balance;
+			_balance = 0;
+			_link.withdraw(amt);
+		}
+	}
+
+	/*
+	 * removes any link from this account
+	 */
+	public void unlink()
+	{
+		if (_link == null)
+			return;
+
+		_link._link = null;
 		_link = null;
-    }
-    
-    
-    /*
-     * gets the minimum value of this account
-     * 
-     * @return the minimum
-     */
-    public double getMinimum() {
-    	return 0;
-    }
-    
-    
-    /*
-     * (non-Javadoc)
-     * @see Filterable#accept()
-     */
-    public boolean accept() {
-    	return true;
-    }
-    
-    
-    
-    
-    //provided methods
-       
-    @Override
-    public boolean equals(Object obj)
-    {
-    	if (obj == null) return false;
-    	
-    	if (!(obj instanceof Account)) return false;
+	}
 
-    	Account that = (Account)obj;
-    	
-    	if (Math.abs(_balance - that._balance) > 0.001) return false; 
+	/*
+	 * gets the minimum value of this account
+	 * 
+	 * @return the minimum
+	 */
+	public double getMinimum()
+	{
+		return 0;
+	}
 
-    	if (!_name.equals(that._name)) return false;
-    	
-    	if (_link != that._link) return false;
-    	
-    	return this == obj;
-    }
-    
-    @Override
-    public String toString()
-    {
-    	String retS = "";
-    	
-    	retS += "account: " + _name + "\n";
-    	retS += "\tBalance: " + _balance + "\n";
-    	retS += "\tLink account name: " + _link._name;
-    	
-    	return retS;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Filterable#accept()
+	 */
+	public boolean accept()
+	{
+		return true;
+	}
+
+	
+	
+	// provided methods
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null)
+			return false;
+
+		if (!(obj instanceof Account))
+			return false;
+
+		Account that = (Account) obj;
+
+		if (Math.abs(_balance - that._balance) > 0.001)
+			return false;
+
+		if (!_name.equals(that._name))
+			return false;
+
+		if (_link != that._link)
+			return false;
+
+		return this == obj;
+	}
+
+	@Override
+	public String toString()
+	{
+		String retS = "";
+
+		retS += "account: " + _name + "\n";
+		retS += "\tBalance: " + _balance + "\n";
+		retS += "\tLink account name: " + _link._name;
+
+		return retS;
+	}
 }
--- Project 6/src/Savings.java
+++ Project 6/src/Savings.java
@@ -1,83 +1,131 @@
 /*
-* @author (Student Name)
-* <p> (File Name)
-* <p> (Assignment)
-* <p> (Describe, in general, the code contained.)
+* @author Anthony Wessel
+* <p> Savings.java
+* <p> Project 6
+* <p> implements a savings account
 */
 public class Savings extends Account
-{	
+{
 	static final double MIN_AMOUNT = 100;
+
 	
-	//
-    // TODO: Constructors and other required methods.
-    //
-	
-	public Savings() {
+	/*
+	 * default constructor
+	 */
+	public Savings()
+	{
 		super();
-		
 	}
+
 	
+	/*
+	 * overloaded constructor
+	 * 
+	 * @param name		name of account
+	 */
 	public Savings(String name)
 	{
 		super(name);
 	}
+
 	
 	
+	/*
+	 * removes money from this account
+	 * 
+	 * @param amt amount to be withdrawn
+	 */
+	@Override
 	public void withdraw(double amt) throws InsufficientFundsException
 	{
+		//check if there is enough money in this account
 		if (_balance - getMinimum() >= amt)
-    	{
-    		_balance -= amt;
-    	}
-    	else
-    	{
-    		if (_link == null) throw new InsufficientFundsException("Overdraft with no linked account");
-    		if (_balance - getMinimum() + _link._balance < amt) throw new InsufficientFundsException("Not enough funds in accounts");
-	    	
-    		amt -= (_balance - getMinimum());
-    		_balance = getMinimum();
-    		_link.withdraw(amt);
-    	}
+		{
+			//withdraw from this account
+			_balance -= amt;
+		}
+		
+		//not enough money in this account
+		else
+		{
+			//check for a linked account
+			if (_link == null)
+				throw new InsufficientFundsException("Overdraft with no linked account");
+			
+			//check if there is enough money in this account and linked account combined
+			if (_balance - getMinimum() + _link._balance < amt)
+				throw new InsufficientFundsException("Not enough funds in accounts");
+
+			//withdraw from both accounts
+			amt -= (_balance - getMinimum());
+			_balance = getMinimum();
+			_link.withdraw(amt);
+		}
 	}
 	
-	
+
+	/*
+	 * gets the minimum value of this account
+	 * 
+	 * @return the minimum
+	 */
 	public double getMinimum()
 	{
-    	return 100;
-    }
+		return 100;
+	}
 
+	
+	/*
+	 * links this account to another account, and vice versa
+	 * 
+	 * @param linkAcct account to link to
+	 * 
+	 * @return whether or not the new link was established
+	 */
+	@Override
+	public boolean link(Account linkAcct)
+	{
+		//check for null parameter
+		if (linkAcct == null)
+			return false;
+
+		//check if linkAcct is another Savings account
+		if (linkAcct instanceof Savings)
+			return false;
+
+		return super.link(linkAcct);
+	}
+
+	
+	/*
+	 * (non-Javadoc)
+	 * @see Account#accept()
+	 */
+	@Override
+	public boolean accept()
+	{
+		return _balance >= getMinimum();
+	}
+
+	
+	
+	// provided methods
 	
 	@Override
-    public boolean link(Account linkAcct)
-    {
-    	if (linkAcct == null) return false;
-    	
-		if (linkAcct instanceof Savings) return false;
-		
-		return super.link(linkAcct);
-    }
-	
-	
-	public boolean accept() {
-    	return _balance >= getMinimum();
-    }
-	
-	
-	
-	
-    @Override
-    public boolean equals(Object obj)
-    {
-    	if (obj == null) return false;
-    	
-    	if (!(obj instanceof Savings)) return false;
+	public boolean equals(Object obj)
+	{
+		if (obj == null)
+			return false;
 
-    	return super.equals(obj);
-    }
-	
-    @Override
-    public String toString()
-    {
-    	return "Savings " + super.toString();
-    }
+		if (!(obj instanceof Savings))
+			return false;
+
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Savings " + super.toString();
+	}
 }
